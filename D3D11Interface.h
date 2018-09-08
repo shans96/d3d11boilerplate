1f086f013f4bd5c43a7fa12205620eaa60203b52
Shan Swanlow <40004835+shans96@users.noreply.github.com>
2018-09-08 13:58:25 +0200

    Update D3D11Interface.h
    Remove unnecessary structs

--- D3D11Interface.h
+++ D3D11Interface.h
@@ -9,24 +9,3 @@ void						ReleaseObjects();
 bool						InitializeScene(); 
 bool						UpdateScene(); 
 bool						DrawScene(); 
-
-struct _DXGI_MODE_DESC {
-  UINT                     Width;
-  UINT                     Height;
-  DXGI_RATIONAL            RefreshRate;
-  DXGI_FORMAT              Format; 
-  DXGI_MODE_SCANLINE_ORDER ScanlineOrdering;
-  DXGI_MODE_SCALING        Scaling;
-};
-
-struct _DXGI_SWAP_CHAIN_DESC {
-	_DXGI_MODE_DESC  BufferDesc;
-	DXGI_SAMPLE_DESC SampleDesc; 
-	DXGI_USAGE       BufferUsage; 
-	UINT             BufferCount; 
-	HWND             OutputWindow; 
-	BOOL             Windowed;
-	DXGI_SWAP_EFFECT SwapEffect; 
-	UINT             Flags; 
-};
-
